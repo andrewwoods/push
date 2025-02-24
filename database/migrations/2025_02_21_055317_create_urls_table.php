1219d92c2fdfe456515647cd0f9a3c4e625f7f73
Andrew Woods <services@awoods.net>
2025-02-24 11:45:25 -0500

    Add alias field to urls migration

--- database/migrations/2025_02_21_055317_create_urls_table.php
+++ database/migrations/2025_02_21_055317_create_urls_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 70);
             $table->string('description', 170);
+            $table->string('alias', 16);
             $table->text('long_url');
             $table->timestamps();
         });
