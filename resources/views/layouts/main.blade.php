7fa1d3f4eb9edc43497bcecb284482bc7a2da5cb
Andrew Woods <services@awoods.net>
2025-02-26 15:19:14 -0500

    Move CSS out of HTML head into app.css
    This keeps the markup clean.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,71 +13,6 @@
     <meta name="description" content="@yield('description')">
     <meta name="author" content="Andrew Woods">
 
-    <!-- Styles -->
-    <style>
-    :root {
-        /* Whitespace Settings */
-        --padding-small: 0.5em;
-        --padding-medium: 1em;
-        --padding-large: 1.5em;
-
-        --margin-small: 0.5em;
-        --margin-medium: 1em;
-        --margin-large: 1.5em;
-
-        /* Color Legend */
-        --navy: #003049;
-        --red: #d62828;
-        --orange: #f77f00;
-        --yellow: #fcbf49;
-        --cream: #eae2b7;
-
-        --white: #ffffff;
-        --black: #2a2a2a;
-
-        /* Font Legend */
-        --sans-font: "Inter Display";
-        --serif-font: "Produkt";
-        --fixed-font: "Noto Sans Mono";
-
-    }
-
-    body {
-        background-color: var(--cream);
-        color: var(--black);
-        font-family: var(--sans-font), sans-serif;
-    }
-
-    h1, h2, h3 {
-        margin-top: 0;
-        margin-bottom: var(--margin-medium);
-        font-family: var(--serif-font), serif;
-    }
-
-    .box-bordered {
-        border: 1px solid var(--navy);
-        padding: var(--padding-medium);
-    }
-
-    label {
-        display: block;
-        margin-top: var(--margin-medium);
-    }
-
-    input, textarea {
-        width: 10em;
-        padding: var(--padding-small);
-        font-size: 1em;
-        font-family: var(--fixed-font), serif;
-    }
-
-    .wide {
-        width: 30em;
-    }
-
-    </style>
-    <!-- Scripts -->
-    <!-- <script src="/js/script.js"></script> -->
 
     <!-- Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -93,7 +28,6 @@
         @yield('content')
     </div>
 </div>
-<!-- <script src="/js/script.js"></script> -->
 </body>
 
 </html>
